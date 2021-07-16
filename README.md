04602be6f14a9a3b6f4c71dd6b81059def43a4d5
ebrandle <esther_brandle@taylor.edu>
2021-07-16 10:49:28 -0700

    restarted work on language translation database and moved it to github

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# samtal
+# Samtal Translation Database
+Spring 2020 – Summer 2021
+
+## Code and Language Creator
+E. Brandle
+
+## Description
+- 
---
+++ samtal_eng_translation.py
@@ -0,0 +1,152 @@
+#Code and language creator: E. Brandle
+#Restarted and brought to GitHub: 2021/07/15
+
+
+##################
+''' TO DO LIST '''
+##################
+'''
++ save english & samtal translations of words
++ list all words or all groups
+- search for translation of word in either direction
+- export database
+- add words to various groups (noun, verb, weather, etc)
+- list all words in a group (tambal alphabetical order)
+- list all groups for a certain word
+- delete word/group
+- update word/group
+- find rhymes
+'''
+
+###################
+''' INTRO STUFF '''
+###################
+import sqlite3
+import csv
+
+menutext = """
+0) Exit program
+A) Add word or part of speech
+L) List words or available parts of speech
+"""
+
+connection = sqlite3.connect('samtal.db')
+cursor = connection.cursor()
+
+
+###########
+''' ADD '''
+###########
+def add():
+    addType = input('\nDo you want to add a word or part of speech (pick one): ').lower()
+    if addType == 'word' or addType == 'words' or addType == 'w':
+        add_word()
+    elif addType == 'pos' or addType == 'part of speech' or addType == 'p':
+        add_pos()
+    else:
+        print('Invalid table name. Please try again.')
+
+def add_word():
+    sam = input('>>> Samtal: ').lower()
+    eng = input('>>> English: ').lower()
+    eng2 = input('>>> Optional 2nd translation: ').lower()
+    values = (sam, eng, eng2)
+    cursor.execute('INSERT INTO words (samtal, english, eng_def_2) VALUES (?,?,?)', values)
+    connection.commit()
+
+def add_pos():
+    p = input('>>> Part of speech: ').lower()
+    values = (p,)
+    cursor.execute('INSERT INTO part_of_speech (pos) VALUES (?)', values)
+    connection.commit()
+
+
+############
+''' LIST '''
+############
+def list_stuff():
+    listType = input('\nDo you want to list a word or part of speech (pick one): ').lower()
+    if listType == 'word' or listType == 'words' or listType == 'w':
+        list_words()
+    elif listType == 'pos' or listType == 'part of speech' or listType == 'p':
+        list_pos()
+    else:
+        print('Invalid table name. Please try again.')
+
+def list_words():
+    print("\n<< Samtal: English >>")
+    for row in cursor.execute('SELECT * FROM words'):
+        tmpRow = row[0] + ': ' + row[1]
+        if row[2] != '':
+            tmpRow = tmpRow + ', ' + row[2]
+        print(tmpRow)
+
+def list_pos():
+    print("Current parts of speech")
+    for row in cursor.execute('SELECT * FROM part_of_speech'):
+        tmpRow = row[0]
+        print(tmpRow)
+
+
+######################
+''' CALL FUNCTIONS '''
+######################
+while True:
+    print(menutext)
+    selection = input('>>> ')
+
+    if selection == '0' or selection == 'q':
+        break
+
+    # add
+    elif selection == 'a':
+        add()
+    elif selection == 'aw':
+        add_word()
+    elif selection == 'ap':
+        add_pos()
+    
+    # list
+    elif selection == 'l':
+        list_stuff()
+    elif selection == 'lw':
+        list_words()
+    elif selection == 'lp':
+        list_pos()
+
+    elif selection == '3':
+        add_word_to_pos()
+        
+    elif selection == '4':
+        list_words_in_pos()
+
+    elif selection == 's':
+        search_word()
+
+    elif selection == 'd':
+        delete()
+
+    elif selection == 'x':
+        export_csv()
+
+    else:
+        print('\nInvalid menu selection. Please try again.')
+
+'''
+CREATE TABLE words (
+    samtal text PRIMARY KEY,
+    english text NOT NULL,
+    eng_def_2 text
+);
+
+CREATE TABLE part_of_speech (
+    pos text PRIMARY KEY
+);
+
+CREATE TABLE link_words_pos (
+    samtal_link text NOT NULL,
+    pos_link text NOT NULL,
+    FOREIGN KEY (samtal_link) REFERENCES words (samtal),
+    FOREIGN KEY (pos_link) REFERENCES part_of_speech (pos)
+);
+'''
